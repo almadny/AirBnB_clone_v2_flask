afa6a5fbd423c68e85fe24c7006fe89d46b0e977
Almadny <Almadny2u@gmail.com>
2023-04-22 12:03:30 +0000

    Fix pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" A Flask Application """
 from flask import Flask
-""" A Flask Application to Print Hello Word """
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def helloWorld():
     """ A function to return the string Hello world """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
